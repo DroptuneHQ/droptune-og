4f9019fbfc9757ca88b2992e53de155c35905b86
Josh Pigford <josh@joshpigford.com>
2018-08-06 20:03:41 -0500

    Initial route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get '/auth/:provider/callback', to: 'sessions#create'
 end
