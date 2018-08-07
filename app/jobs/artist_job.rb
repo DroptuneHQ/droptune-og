8be87dc939de463103d2fc10d4bfdbe0c3bd45f4
Josh Pigford <josh@joshpigford.com>
2018-08-07 17:46:17 -0500

    A bit of cleanup

--- app/jobs/artist_job.rb
+++ app/jobs/artist_job.rb
@@ -14,9 +14,10 @@ class ArtistJob < ApplicationJob
       Artist.with_advisory_lock(artist.id) do
         current_artist = Artist.find_or_create_by(name: artist.name)
         user.artists << current_artist
+
         current_artist.update_attributes spotify_id: artist.id, spotify_followers: artist.followers['total'], spotify_popularity: artist.popularity, spotify_image: artist.images.first['url'], spotify_link: artist.external_urls['spotify']
 
-        AlbumJob.perform_later(current_artist.id) # if current_artist.updated_at > 1.day
+        AlbumJob.perform_later(current_artist.id)# if current_artist.updated_at < 1.day.ago
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ApplicationRecord
   has_many :follows, dependent:  :destroy
   has_many :artists, -> { distinct }, through: :follows
   has_many :albums, through: :artists
+  
   include Storext.model
-
   store_attributes :settings do
     show_compilations Boolean, default: false
     show_singles Boolean, default: false
   end
-
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[twitter spotify]
