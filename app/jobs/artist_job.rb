673a9a134b126e6ee6d2d2273f4d337ba045b02f
Josh Pigford <josh@joshpigford.com>
2018-08-11 16:47:10 -0500

    Trying to fix all this connection pool madness

--- app/jobs/artist_job.rb
+++ app/jobs/artist_job.rb
@@ -3,10 +3,7 @@ class ArtistJob < ApplicationJob
 
   def perform(user_id, artist_ids)
     user = User.find user_id
-    #user.spotify_hash = RSpotify::User.new(user.spotify_hash.to_hash)
     user.save
-
-    #spotify = user.spotify_hash
 
     artists = RSpotify::Artist.find(artist_ids)
 
@@ -17,7 +14,7 @@ class ArtistJob < ApplicationJob
 
         current_artist.update_attributes spotify_id: artist.id, spotify_followers: artist.followers['total'], spotify_popularity: artist.popularity, spotify_image: artist.images.first['url'], spotify_link: artist.external_urls['spotify']
 
-        AlbumJob.perform_later(current_artist.id)# if current_artist.updated_at < 1.day.ago
+        AlbumJob.set(wait: 30.seconds).perform_later(current_artist.id) if current_artist.updated_at > 1.minute.ago or current_artist.updated_at < 1.day.ago
       end
     end
 
