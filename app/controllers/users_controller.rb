c133e1d13a826754700edab0a5ab63d703a5dcb4
Josh Pigford <josh@joshpigford.com>
2018-08-16 14:18:53 -0500

    New music on user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @new_albums = @user.albums.active.has_release_date.order(release_date: :desc, artist_id: :desc).where.not(album_type: 'compilation').uniq.first(12)
   end
 end
