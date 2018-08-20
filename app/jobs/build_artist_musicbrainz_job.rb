f4a8e4529778066bb3a04e50f5913c797542eec5
Josh Pigford <josh@joshpigford.com>
2018-08-20 14:18:24 -0500

    Moar!

--- app/jobs/build_artist_musicbrainz_job.rb
+++ app/jobs/build_artist_musicbrainz_job.rb
@@ -6,7 +6,7 @@ class BuildArtistMusicbrainzJob
 
   sidekiq_throttle({
     # Allow maximum 10 concurrent jobs of this class at a time.
-    :concurrency => { :limit => 3 },
+    :concurrency => { :limit => 5 },
     # Allow maximum 1K jobs being processed within one hour window.
     :threshold => { :limit => 5, :period => 5.seconds }
   })
