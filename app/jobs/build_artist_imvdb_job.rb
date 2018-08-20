97777a08dccd58f94aa742443ae9f7c73947f16a
Josh Pigford <josh@joshpigford.com>
2018-08-20 16:21:17 -0500

    Try speeding things up a bit

--- app/jobs/build_artist_imvdb_job.rb
+++ app/jobs/build_artist_imvdb_job.rb
@@ -6,9 +6,9 @@ class BuildArtistImvdbJob
 
   sidekiq_throttle({
     # Allow maximum 10 concurrent jobs of this class at a time.
-    :concurrency => { :limit => 5 },
+    :concurrency => { :limit => 10 },
     # Allow maximum 1K jobs being processed within one hour window.
-    :threshold => { :limit => 5, :period => 5.seconds }
+    :threshold => { :limit => 8, :period => 5.seconds }
   })
 
   def perform(artist_id)
