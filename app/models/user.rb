bfa821ad3c2dee085d0ce51ed2f9c0a5fdf80906
Josh Pigford <josh@joshpigford.com>
2018-08-23 18:45:58 -0500

    Update Twitter info

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < ApplicationRecord
   end
 
   def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create.tap do | user |
       user.email = provider_data.info.email
       user.username = provider_data.info.nickname
       user.name = provider_data.info.name
