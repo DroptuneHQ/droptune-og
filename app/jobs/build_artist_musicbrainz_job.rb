c01d9dac870f006e9ca3a98d1c51fae0584fb0db
Josh Pigford <josh@joshpigford.com>
2018-08-24 15:26:11 -0500

    Make sure an artist is found

--- app/jobs/build_artist_musicbrainz_job.rb
+++ app/jobs/build_artist_musicbrainz_job.rb
@@ -16,24 +16,27 @@ class BuildArtistMusicbrainzJob
 
     musicbrainz = MusicBrainz::Client.new
     musicbrainz_artist = musicbrainz.artists(artist.name).first
-    musicbrainz_full_artist = musicbrainz.artist musicbrainz_artist.id, includes: 'url-rels'
 
-    urls = musicbrainz_full_artist.urls
-    external_homepage = urls.select{|key| key == 'official homepage'}['official homepage']
-    external_twitter = urls['social network'].select{|key| key.include?("twitter")}.try(:first) if urls['social network'].present?
-    external_facebook = urls['social network'].select{|key| key.include?("facebook")}.try(:first) if urls['social network'].present?
-    external_instagram = urls['social network'].select{|key| key.include?("instagram")}.try(:first) if urls['social network'].present?
-    external_wikipedia = urls['wikipedia']
-    external_youtube = Array.wrap(urls['youtube']).try(:first)
+    if musicbrainz_artist.present?
+      musicbrainz_full_artist = musicbrainz.artist musicbrainz_artist.id, includes: 'url-rels'
 
-    artist.update_attributes(
-      year_started: musicbrainz_artist.date_begin, 
-      year_ended: musicbrainz_artist.try(:date_end),
-      external_homepage: external_homepage, 
-      external_twitter: external_twitter, 
-      external_facebook: external_facebook, 
-      external_instagram: external_instagram,
-      external_wikipedia: external_wikipedia,
-      external_youtube: external_youtube)
+      urls = musicbrainz_full_artist.urls
+      external_homepage = urls.select{|key| key == 'official homepage'}['official homepage']
+      external_twitter = urls['social network'].select{|key| key.include?("twitter")}.try(:first) if urls['social network'].present?
+      external_facebook = urls['social network'].select{|key| key.include?("facebook")}.try(:first) if urls['social network'].present?
+      external_instagram = urls['social network'].select{|key| key.include?("instagram")}.try(:first) if urls['social network'].present?
+      external_wikipedia = urls['wikipedia']
+      external_youtube = Array.wrap(urls['youtube']).try(:first)
+
+      artist.update_attributes(
+        year_started: musicbrainz_artist.date_begin, 
+        year_ended: musicbrainz_artist.try(:date_end),
+        external_homepage: external_homepage, 
+        external_twitter: external_twitter, 
+        external_facebook: external_facebook, 
+        external_instagram: external_instagram,
+        external_wikipedia: external_wikipedia,
+        external_youtube: external_youtube)
+    end
   end
 end
