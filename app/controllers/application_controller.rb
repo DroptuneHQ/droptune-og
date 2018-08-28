68fc3730ea8889c5865276fd9dc73205952714e4
Josh Pigford <josh@joshpigford.com>
2018-08-28 14:11:57 -0500

    Make sure there's a current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
 private
   def set_raven_context
-    Raven.user_context(id: current_user.id, email: current_user.email, username: current_user.username, ip_address: request.ip)
+    Raven.user_context(id: current_user.id, email: current_user.email, username: current_user.username, ip_address: request.ip) if current_user.present?
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
