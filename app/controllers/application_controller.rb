2f26988ff1842c992a09c90e18efad5dfae9bbd5
Josh Pigford <josh@joshpigford.com>
2018-08-28 14:11:32 -0500

    Raven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_raven_context
+
+private
+  def set_raven_context
+    Raven.user_context(id: current_user.id, email: current_user.email, username: current_user.username, ip_address: request.ip)
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
