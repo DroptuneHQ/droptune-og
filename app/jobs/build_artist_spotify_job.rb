a1e9ff8d6b332ba91fd7951f09c20234713a5fd3
Josh Pigford <josh@joshpigford.com>
2018-08-30 16:45:53 -0500

    Slow down spotify a bit

--- app/jobs/build_artist_spotify_job.rb
+++ app/jobs/build_artist_spotify_job.rb
@@ -6,7 +6,7 @@ class BuildArtistSpotifyJob
 
   sidekiq_throttle({
     # Allow maximum 10 concurrent jobs of this class at a time.
-    :concurrency => { :limit => 5 },
+    :concurrency => { :limit => 3 },
     # Allow maximum 1K jobs being processed within one hour window.
     :threshold => { :limit => 5, :period => 5.seconds }
   })
