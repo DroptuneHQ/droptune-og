74306bfca6978d451609719a5448380757a9ac8e
Josh Pigford <josh@joshpigford.com>
2018-09-01 15:35:26 -0500

    Throttle this as well

--- app/jobs/follow_spotify_job.rb
+++ app/jobs/follow_spotify_job.rb
@@ -1,7 +1,15 @@
 class FollowSpotifyJob
   include Sidekiq::Worker
+  include Sidekiq::Throttled::Worker
+  
+  sidekiq_options :queue => :artists
 
-  sidekiq_options :queue => :default
+  sidekiq_throttle({
+    # Allow maximum 10 concurrent jobs of this class at a time.
+    :concurrency => { :limit => 3 },
+    # Allow maximum 1K jobs being processed within one hour window.
+    :threshold => { :limit => 5, :period => 5.seconds }
+  })
 
   def perform(user_id)
     user = User.find user_id
