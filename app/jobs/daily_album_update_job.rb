82e3fdbe07a8eb83fd0eaf1e54e741a80021b5e7
Josh Pigford <josh@joshpigford.com>
2018-09-02 14:40:10 -0500

    Batch the daily update

--- app/jobs/daily_album_update_job.rb
+++ app/jobs/daily_album_update_job.rb
@@ -3,14 +3,19 @@ class DailyAlbumUpdateJob
 
   sidekiq_options :queue => :default
 
-  def perform
-    User.find_each do |user|
+  def perform(batch = 0)
+    User.where("id % 4 = ?", batch).find_each do |user|
       FollowSpotifyJob.perform_async(user.id)
       BuildPlaylistJob.perform_async(user.id)
     end
     
-    Artist.find_each do |artist|
+    Artist.where("id % 4 = ?", batch).find_each do |artist|
       BuildArtistJob.perform_async(artist.id)
     end
   end
 end
+
+# Sidekiq::Cron::Job.create(name: 'Daily update 1', cron: '0 0 * * *', class: 'DailyAlbumUpdateJob', args: 0)
+# Sidekiq::Cron::Job.create(name: 'Daily update 2', cron: '0 6 * * *', class: 'DailyAlbumUpdateJob', args: 1)
+# Sidekiq::Cron::Job.create(name: 'Daily update 3', cron: '0 12 * * *', class: 'DailyAlbumUpdateJob', args: 2)
+# Sidekiq::Cron::Job.create(name: 'Daily update 4', cron: '0 18 * * *', class: 'DailyAlbumUpdateJob', args: 3)
