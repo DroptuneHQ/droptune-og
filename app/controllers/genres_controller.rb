e04fe0a7c94d9e2ab221fb54dc9459c8edc09df3
Josh Pigford <josh@joshpigford.com>
2018-09-03 12:48:14 -0500

    Genre caching

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,10 @@
 class GenresController < ApplicationController
   def index
-    genres = Artist.all.pluck(:genres).flatten.delete_if &:empty?
-    output = Hash[genres.group_by {|x| x}.map {|k,v| [k,v.count]}].sort_by{|k,v| v}.to_h
-    final = output.delete_if {|key, value| value < 35 }
-    @keywords = final
+    @keywords = Rails.cache.fetch("genre-cache", expires_in: 3.hours) do
+      genres = Artist.all.pluck(:genres).flatten.delete_if &:empty?
+      output = Hash[genres.group_by {|x| x}.map {|k,v| [k,v.count]}].sort_by{|k,v| v}.to_h
+      output.delete_if {|key, value| value < 35 }
+    end
   end
 
   def search
