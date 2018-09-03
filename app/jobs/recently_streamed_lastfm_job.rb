3de297ef8086030c72d120b050d19dd9a0b44497
Josh Pigford <josh@joshpigford.com>
2018-09-02 22:43:25 -0500

    Throttling

--- app/jobs/recently_streamed_lastfm_job.rb
+++ app/jobs/recently_streamed_lastfm_job.rb
@@ -1,6 +1,15 @@
 class RecentlyStreamedLastfmJob
   include Sidekiq::Worker
+  include Sidekiq::Throttled::Worker
+
   sidekiq_options :queue => :lastfm
+
+  sidekiq_throttle({
+    # Allow maximum 10 concurrent jobs of this class at a time.
+    :concurrency => { :limit => 5 },
+    # Allow maximum 1K jobs being processed within one hour window.
+    :threshold => { :limit => 1, :period => 1.second }
+  })
 
   def perform(user_id)
     user = User.find user_id
