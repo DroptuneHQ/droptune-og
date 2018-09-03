9c28d0d8db1380b4f9f4bd72e610d7f5d0a93210
Josh Pigford <josh@joshpigford.com>
2018-09-03 12:37:07 -0500

    Encoding fixes limits

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,11 +2,11 @@ class GenresController < ApplicationController
   def index
     genres = Artist.all.pluck(:genres).flatten.delete_if &:empty?
     output = Hash[genres.group_by {|x| x}.map {|k,v| [k,v.count]}].sort_by{|k,v| v}.to_h
-    final = output.delete_if {|key, value| value < 30 }
+    final = output.delete_if {|key, value| value < 35 }
     @keywords = final
   end
 
   def search
-    @artists = Artist.basic_search(genres: params[:genre])
+    @artists = Artist.basic_search(genres: params[:genre]).limit(100)
   end
 end
