029cb07bbfa0c5d89b1b414731540fcba4a4c231
Josh Pigford <josh@joshpigford.com>
2018-09-10 16:31:36 -0500

    Show events if not logged in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,28 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.within(150, origin: current_user.location).includes(:artist).where(artist_id: Follow.select(:artist_id).where(user_id: current_user.id, active: true)).order('starts_at asc').where('starts_at >= ?', Date.today)
+    if current_user
+      @location = current_user.location
+    else 
+      if Rails.env.development?
+        ip = Net::HTTP.get(URI.parse('http://checkip.amazonaws.com/')).squish
+      elsif request.headers["X-Forwarded-For"].present?
+        ip = request.headers["X-Forwarded-For"]
+      else
+        ip = request.remote_ip
+      end
+      
+      loc = Geokit::Geocoders::MultiGeocoder.geocode(ip.to_s)
+      @location = []
+      @location.push(loc.city)
+      @location.push(loc.state)
+      @location.push(loc.country_code)
+      @location = @location.reject(&:blank?).join(', ').to_s
+    end
+
+    if current_user
+      @events = Event.within(150, origin: @location).includes(:artist).where(artist_id: Follow.select(:artist_id).where(user_id: current_user.id, active: true)).order('starts_at asc').where('starts_at >= ?', Date.today)
+    else
+      @events = Event.within(150, origin: @location).includes(:artist).order('starts_at asc').where('starts_at >= ?', Date.today).limit(20)
+    end
   end
 end
