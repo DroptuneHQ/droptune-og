f136be7b8d0d0006ddbda19d93b38f2df9c749f9
Ben Sharpe <bsharpe@gmail.com>
2018-11-16 08:05:21 -0700

    allow app to startup even without these keys

--- config/initializers/spotify.rb
+++ config/initializers/spotify.rb
@@ -1,1 +1,5 @@
-RSpotify.authenticate(ENV['spotify_key'], ENV['spotify_secret'])
+if ENV['spotify_key'] || ENV['spotify_secret']
+	RSpotify.authenticate(ENV['spotify_key'], ENV['spotify_secret'])
+else
+	puts "! please add `spotify_key` and `spotify_secret` to your config/application.yml file to enable Spotify support".yellow
+end
