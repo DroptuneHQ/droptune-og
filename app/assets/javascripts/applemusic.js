fdef326a690e6b3cd3f9b35ecb0fdabbfd094a08
Josh Pigford <josh@joshpigford.com>
2019-11-29 11:03:55 -0600

    Progress

--- app/assets/javascripts/applemusic.js
+++ app/assets/javascripts/applemusic.js
@@ -20,68 +20,64 @@ $(document).on("turbolinks:load", function() {
 
       if (music.isAuthorized) {
         apple_music_id.show().html('<i class="fab fa-apple"></i> Refresh Apple Music Library').addClass('btn btn-light');
-
-        var album = $('.tracks').data('apple-music-album-id');
-        if (album){
-          api_album = music.api.album(album).then(function(item){
-            item.relationships.tracks.data.forEach(function (item, index) {
-              $('.tracks').append('<li><a href="#" class="play_song" data-apple-music-song-id="'+item.attributes.playParams.id+'"><strong class="track_name">'+item.attributes.name+'</strong> <time>'+msToTime(item.attributes.durationInMillis)+'</time></a></li>')
-              console.log(item.attributes, index);
-            });
-          });
-
-          music.setQueue({
-            album: album
-          }).then(function(queue){
-            $(".play").on('click', function(e){
-              e.preventDefault();
-              music.play();
-            });
-
-            $(".pause").on('click', function(e){
-              e.preventDefault();
-              music.pause();
-            });
-
-            $(".next").on('click', function(e){
-              e.preventDefault();
-              music.skipToNextItem();
-            });
-          });
-          
-          $(document).on('click', '.play_song', function(e){
-            e.preventDefault();
-            song = $(this).data('apple-music-song-id');
-            $('.tracks li').removeClass('active');
-            $(this).parent('li').addClass('active');
-            $(this).removeClass('play_song');
-            $(this).addClass('stop_song');
-            console.log(song);
-            music.setQueue({
-              song: song
-            }).then(function(queue){
-              music.play();
-            });
-          });
-
-          $(document).on('click', '.stop_song', function(e){
-            e.preventDefault();
-            song = $(this).data('apple-music-song-id');
-            $('.tracks li').removeClass('active');
-            $(this).removeClass('stop_song');
-            $(this).addClass('play_song');
-            music.setQueue({
-              song: song
-            }).then(function(queue){
-              music.stop();
-            });
-          });
-
-          
-
-        }
-        
       }
+
+      var album = $('.tracks').data('apple-music-album-id');
+      if (album){
+        api_album = music.api.album(album).then(function(item){
+          item.relationships.tracks.data.forEach(function (item, index) {
+            $('.tracks').append('<li><a href="#" class="play_song" data-apple-music-song-id="'+item.attributes.playParams.id+'"><strong class="track_name">'+item.attributes.name+'</strong> <time>'+msToTime(item.attributes.durationInMillis)+'</time></a></li>')
+          });
+        });
+
+        music.setQueue({
+          album: album
+        }).then(function(queue){
+          $(".play").on('click', function(e){
+            e.preventDefault();
+            music.play();
+          });
+
+          $(".pause").on('click', function(e){
+            e.preventDefault();
+            music.pause();
+          });
+
+          $(".next").on('click', function(e){
+            e.preventDefault();
+            music.skipToNextItem();
+          });
+        });
+        
+        $(document).on('click', '.play_song', function(e){
+          e.preventDefault();
+          song = $(this).data('apple-music-song-id');
+          $('.tracks li').removeClass('active');
+          $(this).parent('li').addClass('active');
+          $(this).removeClass('play_song');
+          $(this).addClass('stop_song');
+          
+          music.setQueue({
+            song: song
+          }).then(function(queue){
+            music.play();
+          });
+        });
+
+        $(document).on('click', '.stop_song', function(e){
+          e.preventDefault();
+          song = $(this).data('apple-music-song-id');
+          $('.tracks li').removeClass('active');
+          $(this).removeClass('stop_song');
+          $(this).addClass('play_song');
+          music.setQueue({
+            song: song
+          }).then(function(queue){
+            music.stop();
+          });
+        });
+      }
+
     }
   });
 
