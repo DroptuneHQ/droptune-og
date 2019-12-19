425dacd7fd55b50c20b5ea936768fa36b938b172
Josh Pigford <josh@joshpigford.com>
2019-12-19 13:26:19 -0600

    Weekly reports should use filtered queries

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,16 +10,20 @@ class UserMailer < ApplicationMailer
     @user = User.find user_id
 
     # Albums from past 7 days
-    new_query = Album.includes(:artist).has_release_date.where(artist_id: Follow.select(:artist_id).where(user_id: @user.id, active: true)).order(release_date: :desc)
-    new_query = new_query.where.not(album_type: 'compilation') if !@user.settings['show_compilations']
-    new_query = new_query.where.not(album_type: 'single') if !@user.settings['show_singles']
-    @new_albums = new_query.where("release_date <= ? AND release_date > ?", Date.today, 7.days.ago).uniq.first(9)
+    @new_albums = Album.includes(:artist)
+      .followed_by_user(@user)
+      .filters_for_user(@user)
+      .types_for_user(@user)
+      .default_order.recent_releases(21).limit(9)
 
     # Upcoming albums
-    upcoming_query = Album.includes(:artist).has_release_date.where("release_date > ?", Date.today).where(artist_id: Follow.select(:artist_id).where(user_id: @user.id, active: true)).order(release_date: :asc)
-    upcoming_query = upcoming_query.where.not(album_type: 'compilation') if !@user.settings['show_compilations']
-    upcoming_query = upcoming_query.where.not(album_type: 'single') if !@user.settings['show_singles']
-    @upcoming_albums = upcoming_query.uniq.first(9)
+    @upcoming_albums = Album.includes(:artist)
+      .future_releases
+      .followed_by_user(@user)
+      .filters_for_user(@user)
+      .types_for_user(@user)
+      .order('release_date asc', 'artists.name asc')
+      .limit(9)
 
     if @new_albums.present? || @upcoming_albums.present?
       mail(to: @user.email, subject: "Droptune New Music: #{Date.today.strftime('%b %-d, %Y')}")
