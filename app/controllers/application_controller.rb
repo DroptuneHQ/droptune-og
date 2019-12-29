1c09ad900ddb1792c9437d447d3c59f869160187
Daniel Westendorf <daniel@prowestech.com>
2019-12-29 15:46:36 -0700

    Add some helper methods to identify if the client is a mobile app
    Could be used later when taking advantage of rail's variants to provide unique views for the mobile apps.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   include ExceptionHandler
   before_action :set_raven_context
+  helper_method :mobile_app?
 
 private
   def set_raven_context
     Raven.user_context(id: current_user.id, email: current_user.email, username: current_user.username, ip_address: request.ip) if current_user.present?
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+
+  def mobile_app?
+    request.user_agent.to_s.include?('Droptune - MobileApp')
+  end
+
+  def ios_mobile_app?
+    mobile_app? && request.user_agent.to_s.include?('iOS')
+  end
+
+  def mobile_app_version?
+    request.user_agent.to_s.split('version::').last || '0.1 Build(0)'
   end
 end
