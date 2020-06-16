3604ccabff573d070a1652e4453d111f71ba0fee
Josh Pigford <josh@joshpigford.com>
2020-06-16 12:39:13 -0500

    Remove Raven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
   include ExceptionHandler
-  before_action :set_raven_context
   helper_method :mobile_app?
 
 private
-  def set_raven_context
-    Raven.user_context(id: current_user.id, email: current_user.email, username: current_user.username, ip_address: request.ip) if current_user.present?
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
-
   def mobile_app?
     @mobile_app ||= request.user_agent.to_s.include?('Droptune - MobileApp')
   end
